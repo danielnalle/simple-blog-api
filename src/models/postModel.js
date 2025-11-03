11c56e218e15700b7e389a35707ee3b2caa8e1b9
danielnalle <danielnalle31@gmail.com>
2025-11-03 21:27:32 +0800

    feat(post): Menambahkan model findAllPosts untuk mengambil semua postingan

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -8,3 +8,20 @@ export const createPost = async (title, content, userId) => {
 
   return newPost.rows[0];
 };
+
+export const findAllPosts = async (page = 1, limit = 10) => {
+  const offset = (page - 1) * limit;
+
+  const countResult = await db.query("SELECT COUNT(*) FROM posts");
+  const totalItems = parseInt(countResult.rows[0].count, 10);
+
+  const postsResult = await db.query(
+    `SELECT p.id, p.title, p.created_at, u.username AS author_username FROM posts p LEFT JOIN users u ON p.user_id = u.id ORDER BY p.created_at DESC LIMIT $1 OFFSET $2`,
+    [limit, offset]
+  );
+
+  return {
+    posts: postsResult.rows,
+    totalItems: totalItems,
+  };
+};
