05b008cb211a10605f0a3dde3601d16e6b950e9a
danielnalle <danielnalle31@gmail.com>
2025-11-03 10:50:36 +0800

    chore(db): Menambahkan logging pada database

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,10 @@ import { Pool } from "pg";
 
 const pool = new Pool();
 
-export const query = (text, params = []) => {
-  return pool.query(text, params);
+export const query = async (text, params = []) => {
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+  console.log("executed query", { text, duration, rows: res.rowCount });
+  return res;
 };
