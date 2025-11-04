6b70e9ea938789e7554acc88805d004fea954d41
danielnalle <danielnalle31@gmail.com>
2025-11-04 19:29:22 +0800

    feat(post): Tambah endpoint untuk mengupdate suatu postingan berdasarkan id postingan dan author

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,9 @@
-import { createPost, findAllPosts, findPostById } from "../models/postModel.js";
+import {
+  createPost,
+  findAllPosts,
+  findPostById,
+  updatePost,
+} from "../models/postModel.js";
 
 export const postPost = async (req, res) => {
   const { title, content } = req.body;
@@ -77,20 +82,20 @@ export const getPosts = async (req, res) => {
 };
 
 export const getPostById = async (req, res) => {
-  const { id } = req.params;
-
-  const postId = parseInt(id, 10);
-  if (isNaN(postId)) {
-    return res.status(400).json({
-      status: "fail",
-      error: {
-        code: "INVALID_INPUT",
-        message: "ID Post harus berupa angka.",
-      },
-    });
-  }
-
   try {
+    const { id } = req.params;
+
+    const postId = parseInt(id, 10);
+    if (isNaN(postId)) {
+      return res.status(400).json({
+        status: "fail",
+        error: {
+          code: "INVALID_INPUT",
+          message: "ID Post harus berupa angka.",
+        },
+      });
+    }
+
     const postData = await findPostById(postId);
 
     if (!postData) {
@@ -106,6 +111,62 @@ export const getPostById = async (req, res) => {
     return res.status(200).json({
       status: "success",
       data: postData,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      status: "error",
+      error: {
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Terjadi kesalahan pada server.",
+      },
+    });
+  }
+};
+
+export const putPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const { id: userId } = req.user;
+
+    const postId = parseInt(id, 10);
+    if (isNaN(postId)) {
+      return res.status(400).json({
+        status: "fail",
+        error: {
+          code: "INVALID_INPUT",
+          message: "ID Post harus berupa angka.",
+        },
+      });
+    }
+
+    if (!title || !content || title.trim() === "" || content.trim() === "") {
+      return res.status(400).json({
+        status: "fail",
+        error: {
+          code: "VALIDATION_ERROR",
+          message: "Judul dan konten tidak boleh kosong",
+        },
+      });
+    }
+
+    const updatedPostResult = await updatePost(postId, title, content, userId);
+
+    if (updatedPostResult.rowCount === 0) {
+      return res.status(404).json({
+        status: "fail",
+        error: {
+          code: "RESOURCE_NOT_FOUND",
+          message: `Postingan dengan ID ${postId} tidak ditemukan atau Anda tidak memiliki akses.`,
+        },
+      });
+    }
+
+    return res.status(200).json({
+      status: "success",
+      data: updatedPostResult.rows[0],
     });
   } catch (error) {
     console.error(error);
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -3,6 +3,7 @@ import {
   postPost,
   getPosts,
   getPostById,
+  putPostById,
 } from "../controllers/postController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/", authMiddleware, postPost);
 router.get("/", getPosts);
 router.get("/:id", getPostById);
+router.put("/:id", authMiddleware, putPostById);
 
 export default router;
